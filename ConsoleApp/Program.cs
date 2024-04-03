392f5f352d0159efb4a43f9b0991e91ef0745267
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-04-03 18:42:25 +0500

    Demo app

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Numerics;
+
+
+
+//Console.Beep(10000, 5000);
+Console.ForegroundColor=ConsoleColor.Green;
+Console.WriteLine("Hello");
+
+DateTime dateTime = new DateTime(2023, 07, 26);
+Console.WriteLine(dateTime.DayOfYear);
+
+DateTime afterTendayDateTime = dateTime.AddDays(48);
+Console.WriteLine(afterTendayDateTime.DayOfYear);
+
+Console.WriteLine("Hello world");
+ConsoleKeyInfo c=Console.ReadKey();
+Console.WriteLine(c.Key);
+
+if(c.Key==ConsoleKey.Enter)
+{
+
+    Console.BackgroundColor = ConsoleColor.White;
+    Console.WriteLine("Blue");
+}
+
+
+var nums = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+foreach (var item in nums)
+{
+    Console.Write(item+1+" ");
+}
