947752df354bbcdf3c64660f28c0976232967722
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-04-20 07:21:26 +0500

    OOP Amaliyot

---
+++ OOP_Amaliyot/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System.Text;
+
+string s = "salom";
+s = s + " dunyo";
+
+StringBuilder strbuilder= new StringBuilder();
+strbuilder.Append("Salom");
+strbuilder.Append("Dunyo");
+
+Console.WriteLine(s);
+Console.WriteLine(strbuilder);
