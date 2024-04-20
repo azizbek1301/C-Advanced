0694a57a9a1c9ac57f3cff51d23dad1589f30502
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-04-20 07:22:23 +0500

    Interfaces

---
+++ ParamsKeyword/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+int[] numbers = new int[] { 1, 4, 5, 6, 7, 6, 9, 4, 6, 6, 6, 5, 6 };
+
+Console.WriteLine(Sum(numbers));
+
+static int Sum(params int[] sonlarRoyxati)
+{
+    int sum = 0;
+    foreach (var item in sonlarRoyxati)
+    {
+        sum += item;
+    }
+    return sum;
+}
+
+
+//if (args[0]=="Salom")
+//    Console.WriteLine("Salom dunyo");
+//else if (args[0]=="5")
+//{
+
+//}
