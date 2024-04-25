9e2052b7ec9c2697bfb554b81bc43f143e8b091f
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-04-25 14:29:30 +0500

    Delegate

--- OOP(interfacelar .)/Program.cs
+++ OOP(interfacelar .)/Program.cs
@@ -1,5 +1,73 @@
 ﻿
+#region Collections
 
-Dictionary<int,string> weekdays=new Dictionary<int,string>();
+//Dictionary<string,string> weekdays=new Dictionary<string,string>();
 
-weekdays.Add(1, "Dushanba");
+//weekdays.Add("Uzbekistan", "Tashkent");
+//weekdays.Add("Rjussia", "Moskow");
+//weekdays.Add("Usa", "Washington");
+
+//Console.WriteLine(weekdays["Usa"]);
+
+//Stack<int> numbers = new Stack<int>();
+
+//numbers.Push(1);
+//numbers.Push(12);
+//numbers.Push(15);
+
+//foreach (var num in numbers)
+//{
+//    Console.WriteLine(num + " ");
+//}
+
+//static void PrintValuesOfStack()
+//{
+
+//}
+
+#endregion
+
+#region Delegatlar
+
+
+//static int Add(int a, int b) => a + b;
+
+
+
+//Do add = Add;
+//public delegate int Do(int a, int b);
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+
+        Func<string,int, char ,bool> check = (s,i,c) => s[i]==c;
+        
+
+        Action<string> print = (s)=> Console.WriteLine(s);
+
+
+        print(check("Azizbek", 2, 'i').ToString());
+    }
+
+    static void ConvertAll(int[] ar,Action<int> action)
+    {
+        foreach(var number in ar)
+        {
+            action(number);
+        }
+    }
+
+    
+  
+}
+
+
+
+
+
+
+
+#endregion
