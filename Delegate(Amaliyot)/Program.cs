275eecbcc448890c05a9752594d7ea1871978c99
Azizbek Rakhmatullayev <azizbekrakhmatullayev1317@gmail.com>
2024-04-27 12:59:26 +0500

    Delegate

---
+++ Delegate(Amaliyot)/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+public class Program
+{
+    
+
+    static void Main(string[] args)
+    {
+        
+        Func<int, int, int> func = new Func<int, int, int>(Add);
+
+
+    }
+
+   
+
+    static int Add(int a, int b)
+    {
+        return a + b ;
+    }
+
+    static int Subtract(int a,int b)
+    { 
+        return a - b;
+    }
+}
